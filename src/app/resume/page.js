1ce1bedfb6e49cedfab8b82abc0546c54837ede4
Kmeadows3 <kmeadows3@gmail.com>
2024-10-14 16:01:26 -0400

    Added resume page and some formatting

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,11 +1,64 @@
+import { resumeData } from "@/data/resume"
+import styles from '@/app/page.module.css'
+import Link from "next/link"
+
+
 export const metadata = {
     title: 'Resume',
 }
 
-export default function Resume(){
-    return(
-        <>
-        Resume Page
-        </>
+export default function Resume() {
+    return (
+        <div className={styles.resume}>
+            <h1 className={styles.title}>Resume</h1>
+            <h2 className={styles.name}>Katherine Meadows</h2>
+            <h2 className={styles.subsectionTitle}>Contact Information</h2>
+            <div className={styles.contact}>
+                <span><strong>Email: </strong><Link href={resumeData.contactInfo.email}>{resumeData.contactInfo.email}</Link></span>
+                <span><strong>Phone: </strong>{resumeData.contactInfo.phone}</span>
+                <span><strong>LinkedIn: </strong><Link href={resumeData.contactInfo.linkedIn}>kmeadows3</Link></span>
+                <span><strong>GitHub: </strong><Link href={resumeData.contactInfo.github}>kmeadows3</Link></span>
+                <span><strong>Location: </strong>{resumeData.contactInfo.location}</span>
+            </div>
+            <h2 className={styles.subsectionTitle}>Technical Experience</h2>
+            <div>
+                {resumeData.technicalExperience.map(experience => (
+                    <div key={experience.name}>
+                        <h3 className={styles.entryTitle}>{experience.name}</h3>
+                        <p>{experience.description}</p>
+                    </div>
+                ))}
+            </div>
+            <h2 className={styles.subsectionTitle}>Professional Experience</h2>
+            <div>
+                {resumeData.professionalExperience.map(experience => (
+                    <div key={experience.company + '-' + experience.role}>
+                        <h3 className={styles.experienceHeader}>
+                            <span className={styles.company}>{experience.company}</span><span className={styles.role}>{experience.role}</span>
+                            <div>
+                                <span>{experience.location}</span>
+                                <span>{experience.startDate} - {experience.endDate}</span>
+                        </div>
+                        </h3>
+                        
+                        <ul>
+                            {experience.highlights.map(highlight => (
+                                <li key={highlight}>{highlight}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+            <h2 className={styles.subsectionTitle}>Education</h2>
+            <div>
+                {resumeData.Education.map(education => (
+                    <div key={education.name}>
+                        <p className={styles.entryTitle}><span><strong>{education.name}</strong> | {education.location}</span> <span>{education.dates}</span></p>
+                        <p>{education.description}</p>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.footer}>&nbsp;</div>
+        </div>
     )
 }
---
+++ src/data/resume.js
@@ -0,0 +1,93 @@
+export const resumeData = {
+    name: 'Katherine Meadows',
+    contactInfo: {
+        email: 'kmeadows3@gmail.com',
+        phone: '614-736-8248',
+        linkedIn: 'https://www.linkedin.com/in/kmeadows3/',
+        github: 'https://github.com/kmeadows3',
+        location: 'Columbus, OH'
+    },
+    summary: 'Software developer with a background in science seeking to change careers after graduation from a coding boot camp. Eager to contribute analytical and problem-solving skills to a challenging role while continuing to grow as a developer.',
+    technicalSkills: [
+        'Java', 'Spring Boot', 'JavaScript', 'Vue.js', 'PostgreSQL', 'HTML', 'CSS', 'Responsive Design',  'IntelliJ', 'Git', 'Unit Testing (JUnit)', 'E/R diagrams', 'Integration Testing', 'Agile', 'Table Design', 'JDBC', 'Excel', 'Problem Solving', 'Object-Oriented Programming', 'Data Analysis','Software Development'
+    ],
+    technicalExperience: [
+        {
+            name: 'Tech Elevator Chatbot',
+            description: 'Worked with a small team using Agile methodologies to develop Chatwick, a rules-based chatbot. A Vue.js front-end sends user utterances to a RESTful Java application using Spring Boot to run a Tomcat server. Chatbot responses are retrieved from a PostgreSQL database, then filtered and ranked to determine the most fitting response.'
+        },
+        {
+            name: 'Tabletop Wargame Team Builder',
+            description: 'Created a team builder for the indie tabletop wargame ’This is Not a Test.’ The application is deployed on Microsoft Azure and stores teams on PostgreSQL database. The front-end is a Static Web App that uses Vue.js and communicates through HTTPS with a Java API App to perform data validation and updates. Testing performed using JUnit and Postman.'
+        },
+        {
+            name: 'TEnmo Application',
+            description: 'Applied the principles of Object Oriented Programming to develop a money transfer application. The project consisted of a command-line client connected to a Spring Boot server through a REST API and a PostgreSQL database.'
+        }
+    ],
+    professionalExperience: [
+        {
+            company: 'Goldberg Properties LLC',
+            role:'Operations Manager',
+            startDate: 'Jan 2020',
+            endDate: 'Present',
+            location: 'Columbus, OH',
+            highlights: [
+                'Oversee the launch and day-to-day operations of 15 different short term rentals across Columbus',
+                'Cultivate positive working relationships with both customers and contractors working for the company',
+                'Established new processes and procedures as the company changed industries from real estate into hospitality'
+            ]
+        },
+        {
+            company: 'Goldberg Properties LLC',
+            role:'Real Estate Assistant',
+            startDate: 'Mar 2015',
+            endDate: 'Jan 2020',
+            location: 'Columbus, OH',
+            highlights: [
+                'Analyzed up to 100 properties weekly as potential investments both as rentals and for renovation and resale',
+                'Acted as assistant property manager for 17 different student rentals',
+                'Automated data extraction from a list of properties, reducing analysis time by 2-3 hours per week'
+            ]
+        },
+        {
+            company: 'Marshall D. Cohen LLC',
+            role:'Marketing Specialist',
+            startDate: 'Mar 2017',
+            endDate: 'Dec 2019',
+            location: 'Columbus, OH',
+            highlights: [
+                'Extracted data from public databases to identify potential clients and customize marketing material for them',
+                'Developed a filtering system for potential clients, reducing unwanted and duplicate mailings by 90%'
+            ]
+        },
+        {
+            company: 'The OSU Comprehensive Cancer Center',
+            role:'Clinical Research Data Coordinator',
+            startDate: 'Aug 2014',
+            endDate: 'Aug 2016',
+            location: 'Columbus, OH',
+            highlights: [
+                'Managed data entry on 5 competing clinical trial protocols for the James Cancer Center, sometimes training and supervising teams of other coordinators to ensure deadlines were met',
+                'Created a tracking system for protocols, increasing my efficiency to more than double the target for a full-time workload',
+                'Pioneered changes to streamline data entry, reducing the workload on one protocol by over 33%'
+            ]
+        }
+    ],
+    Education: [
+        {
+            name: 'Tech Elevator',
+            location: 'Columbus, OH',
+            description: 'Full-stack boot camp designing software systems and solutions with 800+ hours of education and application',
+            dates: 'Jan 2024 - Apr 2024'
+        },
+        {
+            name: 'Georgia Institute of Technology',
+            location: 'Atlanta, GA',
+            description: 'BS in Biology',
+            dates: 'Aug 2006 - Dec 2011'
+        },
+    ]
+        
+
+}
